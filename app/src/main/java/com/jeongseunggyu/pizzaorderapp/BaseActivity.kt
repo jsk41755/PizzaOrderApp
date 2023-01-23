2c18a10d879e797a67d4064acd10ee95b58f3e6f
jsk41755 <jsk41755@naver.com>
2023-01-23 17:14:14 +0900

    BaseActivity 추가

--- app/src/main/java/com/jeongseunggyu/pizzaorderapp/BaseActivity.kt
+++ app/src/main/java/com/jeongseunggyu/pizzaorderapp/BaseActivity.kt
@@ -1,4 +1,12 @@
 package com.jeongseunggyu.pizzaorderapp
 
-class BaseActivity {
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
 }
